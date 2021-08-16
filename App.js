bb8a244e8d906f0eab3e8e39fa8910cfbd369f79
YASH <46160689+theyashjaiswal@users.noreply.github.com>
2021-08-16 01:47:00 -0300

    init commit

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text
+          style={{
+            color: "black",
+            fontWeight: "bold",
+            fontSize: 53,
+          }}
+        >
+          {" "}
+          Let's GO! 🧨 REACT NATIVE👨🏻‍💻{" "}
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  baseText: {
+    fontWeight: "bold",
+  },
+  innerText: {
+    color: "red",
   },
 });
